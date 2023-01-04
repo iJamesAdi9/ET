4b00bf8e50fdf17b7cebda298af6f19a3fb2bfbb
AKAZAR ZEMO <wachiravit.teer@kkpfg.com>
2023-01-05 02:03:45 +0700

    First commit

---
+++ ET/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ET
+//
+//  Created by Wachiravit Teerasarn on 5/1/2566 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ET/ETApp.swift
@@ -0,0 +1,17 @@
+//
+//  ETApp.swift
+//  ET
+//
+//  Created by Wachiravit Teerasarn on 5/1/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct ETApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
